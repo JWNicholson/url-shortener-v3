67e163864d0387bb8def3c7bd488d424d5997118
JW Nicholson <john.nicholson812@gmail.com>
2020-10-29 15:52:50 -0400

    app almost working:short links do not work. check antivirus for conflict

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ connectDB();
 //middleware
 app.use(express.json({extented:false}));
 
+// define routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
 const PORT = 5000;
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
